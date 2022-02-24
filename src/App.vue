dbb3d987db923ca7e26b9021d799fdd36444e151
josumx <josue@insist.com.mx>
2022-02-23 20:41:11 -0600

    avances

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="assets/strommda.jpg"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <div class="d-flex align-center"></div>
 
       <v-spacer></v-spacer>
 
@@ -34,19 +16,19 @@
     </v-app-bar>
 
     <v-main>
-      <entrada />
+      <pago />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import entrada from "./components/entrada";
+import pago from "./components/pago";
 
 export default {
   name: "App",
 
   components: {
-    entrada,
+    pago,
   },
 
   data: () => ({
--- src/components/pago.vue
+++ src/components/pago.vue
@@ -11,29 +11,26 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Bienvenido
-        </h1>
-        <h2 class="headline font-weight-bold mb-3">
-          Acerca su mano al sensor para obtener boleto
-        </h2>
-
-
+        <h1 class="display-2 font-weight-bold mb-3">Pago</h1>
+        <h2 class="headline font-weight-bold mb-3">Escanee su boleto</h2>
+        <v-text-field
+          label="No. de boleto"
+          placeholder="Escanee su boleto"
+          on:keyup.enter="onEnter"
+        ></v-text-field>
       </v-col>
-
-
     </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-
-    data: () => ({
-
-
-
-    }),
-  }
+export default {
+  name: "parkingPago",
+  data: () => ({}),
+  methods: {
+    onEnter: function (e) {
+      console.log(e);
+    },
+  },
+};
 </script>
